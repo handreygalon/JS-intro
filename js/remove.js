55055628e10caee3ed4d4cf234d1bddb9d94fa93
handreygalon <handrey.eg@gmail.com>
2019-09-20 08:19:14 -0300

    # Adding animation on patient removal

--- js/remove.js
+++ js/remove.js
@@ -3,10 +3,18 @@ var patients = document.querySelectorAll(".paciente");
 var table = document.querySelector("table");
 
 table.addEventListener("dblclick", function(event) {
+    //this -> event owner | target -> who suffered the event
     //var eventTarget = event.target;
     //var fatherTarget = eventTarget.parentNode;
     //fatherTarget.remove();
-    event.target.parentNode.remove();
+    //event.target.parentNode.remove();
+
+    // Add animation when remove a patient
+    event.target.parentNode.classList.add("fadeOut");
+    setTimeout(function() {
+        event.target.parentNode.remove();
+    }, 500);
+    
 });
 
 /*patients.forEach(function(patient) {
