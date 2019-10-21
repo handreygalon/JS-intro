e5386a1d50f02c85adb87e90c7006c49f5b42c2f
handreygalon <handrey.eg@gmail.com>
2019-10-21 08:25:28 -0300

    # Handling request errors

--- js/search.js
+++ js/search.js
@@ -5,12 +5,20 @@ addButton.addEventListener("click", function() {
 
     xhr.open("GET", "http://api-pacientes.herokuapp.com/pacientes");  // Which method HTTP we will use, in this case: GET
     xhr.addEventListener("load", function() {
-        var response = xhr.responseText;
-        var patients = JSON.parse(response);
+        var errorAjax = document.querySelector("#error-ajax");
+        errorAjax.classList.add("invisible");
+        if (xhr.status == 200) {
+            var response = xhr.responseText;
+            var patients = JSON.parse(response);
 
-        patients.forEach(function(patient) {
-            addPatientToTable(patient);
-        });
+            patients.forEach(function(patient) {
+                addPatientToTable(patient);
+            });
+        } else {
+            console.log(xhr.status);
+            console.log(xhr.responseText);
+            errorAjax.classList.remove("invisible");
+        }        
     });
     xhr.send();
 });
